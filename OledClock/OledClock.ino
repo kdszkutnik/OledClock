c9c9caf44bb8ada7834c6c91f225d3580a74929e
kdszkutnik <kdszkutnik@gmail.com>
2021-12-21 22:15:15 +0100

    ShowColon() fixed

--- OledClock/OledClock.ino
+++ OledClock/OledClock.ino
@@ -5,9 +5,7 @@ OledDisplay display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 void setup() {
   Serial.begin(9600);
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
 }
 
 void loop() {
--- OledClock/OledDisplay.cpp
+++ OledClock/OledDisplay.cpp
@@ -1,6 +1,7 @@
 #include "OledDisplay.h"
 
 void OledDisplay::showTime(int hours, int minutes) {
+  clearDisplay();
   setTextSize(4);
   setTextColor(WHITE);
   setCursor(4, 15);
@@ -18,10 +19,14 @@ char OledDisplay::showColon(void) {
 
     if(showColonBool) {
       showColonBool = 0;
-      return ':';
     } else {
       showColonBool = 1;
-      return ' ';
     }
+  }
+  
+  if(showColonBool) {
+    return ':';
+  } else {
+    return ' ';
   }
 }
